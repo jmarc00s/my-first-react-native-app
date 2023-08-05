44ea4ba78c27f1c8e67350e18bfcb6ae5b20dfab
João Marcos <jmarcossferreira@hotmail.com>
2023-08-05 11:00:17 -0300

    add imc calculation

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 import { Imc } from "./components/Imc";
 
 export const Form = () => {
+  const [form, setForm] = useState({
+    height: "",
+    weight: "",
+  });
+
+  const [imc, setImc] = useState(0);
+
+  const handleImcCalculation = () => {
+    const heightSquared = +form.height * +form.height;
+    setImc((+form.weight / heightSquared).toFixed(2));
+    console.log(+form.weight);
+  };
+
+  const handleFormChange = (text, name) =>
+    setForm((prevState) => ({ ...prevState, [name]: text }));
+
   return (
     <View>
-      <View>
+      <View style={styles.container}>
         <Text>Altura</Text>
-        <TextInput placeholder="Ex: 1.83" keyboardType="numeric" />
+        <TextInput
+          value={form.height}
+          placeholder="Ex: 1.83"
+          keyboardType="numeric"
+          onChangeText={(text) => handleFormChange(text, "height")}
+        />
         <Text>Peso</Text>
-        <TextInput placeholder="Ex: 75.5" keyboardType="numeric" />
-        <Button title="Calcular IMC" />
+        <TextInput
+          value={form.weight}
+          id="weight"
+          onChangeText={(text) => handleFormChange(text, "weight")}
+          placeholder="Ex: 75.5"
+          keyboardType="numeric"
+        />
+        <Button onPress={handleImcCalculation} title="Calcular IMC" />
       </View>
-      <Imc message="IMC" imc={50} />
+
+      {!!imc && <Imc message="IMC" imc={imc} />}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    gap: 8,
+  },
+});
